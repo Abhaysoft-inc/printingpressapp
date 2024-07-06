8b84bb127bc9c902b82e8bf667c39a4ba4af2632
Abhay Vishwakarma <av385735@gmail.com>
2024-07-06 18:05:55 +0530

    abhay

--- printingpresssite/src/components/HelpSection.jsx
+++ printingpresssite/src/components/HelpSection.jsx
@@ -47,15 +47,15 @@ export default function HelpSection() {
                 <div className="flex flex-col md:flex-row justify-between mx-4 md:mx-44 mt-16 space-y-6 md:space-y-0 md:space-x-6">
                     <div className="box1">
                         <div className="flex justify-center">
-                            <BiSupport className="w-20 h-20 md:w-40 md:h-40" />
+                            <BiSupport className="w-20 h-20 md:w-40 md:h-40" color='#0c366e' />
                         </div>
                         <p className="text-center font-semibold text-lg mt-1 text-gray-800">Call us for queries</p>
-                        <p className="text-center text-gray-600">Helpdesk: <b> +91 9696745350</b></p>
+                        <p className="text-center text-gray-600">Helpdesk: <b> +91 9696745350, +91 9125318307</b></p>
                         <p className="text-center text-gray-600">(Mon - Sat: 10:00 AM - 7:00 PM)</p>
                     </div>
                     <div className="box2">
                         <div className="flex justify-center">
-                            <MdOutlineEmail className="w-20 h-20 md:w-40 md:h-40" />
+                            <MdOutlineEmail className="w-20 h-20 md:w-40 md:h-40" color='#0c366e' />
                         </div>
                         <p className="text-center font-semibold text-lg mt-1 text-gray-800">E-Mail us</p>
                         <p className="text-center text-gray-600">Sales enquiries and customer support:</p>
@@ -63,7 +63,7 @@ export default function HelpSection() {
                     </div>
                     <div className="box1">
                         <div className="flex justify-center">
-                            <IoIosPin className="w-20 h-20 md:w-40 md:h-40" />
+                            <IoIosPin className="w-20 h-20 md:w-40 md:h-40" color='#0c366e' />
                         </div>
                         <p className="text-center font-semibold text-lg mt-1 text-gray-800">Postal address</p>
                         <p className="text-center text-gray-600">Karvi Printing Press Pvt. Ltd.</p>
