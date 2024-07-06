70e69d3a4b494a7b9eb3e2a359b572563b0c6525
Abhay Vishwakarma <av385735@gmail.com>
2024-07-06 18:09:56 +0530

    gf

--- printingpresssite/src/components/HelpSection.jsx
+++ printingpresssite/src/components/HelpSection.jsx
@@ -6,40 +6,6 @@ import { IoIosPin } from "react-icons/io";
 export default function HelpSection() {
     return (
         <>
-
-            {/* <div className="help mt-10 mb-16">
-                <h2 className="about text-center text-3xl font-Poppins font-semibold mt-14">Help Center</h2>
-                <div className="flex justify-between mx-44 mt-16 space-x-6">
-
-                    <div className="box1 ">
-                        <div className="flex justify-center">
-                            <BiSupport size={160} /></div>
-                        <p className="text-center font-[600] text-lg mt-1">Call us for queries</p>
-                        <p className="text-center text-[#666666]">Helpdesk: +91 99999999</p>
-                        <p className="text-center text-[#666666]">(Mon - Sat: 10:00 AM - 7:00 PM)</p>
-
-                    </div>
-                    <div className="box2 ">
-                        <div className="flex justify-center">
-                            <MdOutlineEmail size={160} /></div>
-                        <p className="text-center font-[600] text-lg mt-1">E-Mail us</p>
-                        <p className="text-center text-[#666666]">Sales enquiries and customer support:</p>
-                        <p className="text-center text-[#666666]">Karviprinting.helpcare@gmail.com</p>
-                    </div>
-                    <div className="box1 ">
-                        <div className="flex justify-center">
-                            <IoIosPin size={160} /></div>
-                        <p className="text-center font-[600] text-lg mt-1">Postal address</p>
-                        <p className="text-center text-[#666666]">Karvi Printing Press Pvt. Ltd.
-                        </p>
-                        <p className="text-center text-[#666666]">Narayan Puram Colony, Padari Bazar, <br /> Gorakhpur Uttar Pradesh - 273014</p>
-                    </div>
-
-                </div>
-
-            </div> */}
-
-
             <div className="help mt-10 mb-16">
                 <h2 className="about text-center text-3xl font-Poppins font-semibold mt-14 text-gray-800">
                     Help Center
